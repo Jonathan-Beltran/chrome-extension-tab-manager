44a90602c92e319c5b44c0dc9d54f5c34eb449bf
Jonathan-Beltran <python8utoo@gmail.com>
2024-03-06 11:11:01 -0500

    Fixes querying all tabs and adding them to the list

--- src/popup.js
+++ src/popup.js
@@ -1,11 +1,19 @@
-const tabs = await chrome.tabs.query({
-    url: [
-        "*://*/*"
-    ]
-});
+ async function queryTabs(){
+    const tabs = await chrome.tabs.query({
+        url: [
+            "*://*/*"
+        ]
+    });
 
-//const collator = new Intl.Collator();
-for (const tab of tabs){
-    const title = tab.title;
+     const tabsList = document.querySelector('#tabsList')
+     for (const tab of tabs) {
+         const template = document.querySelector('#li_template').content.cloneNode(true);
 
+         //fill template with tabs details
+         template.querySelector('.title').textContent = tab.title;
+         template.querySelector('.pathname').textContent = tab.url;
+         template.querySelector('a').href = tab.url;
+         tabsList.appendChild(template);
+     }
 }
+document.addEventListener('DOMContentLoaded', queryTabs);
