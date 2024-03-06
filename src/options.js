b24bd7ed70b7993da25e35a4a14ff237b1027cf2
Jonathan-Beltran <python8utoo@gmail.com>
2024-03-06 00:43:34 -0500

    Basic functionality of minutes dropdown menu in options

--- src/options.js
+++ src/options.js
@@ -1,10 +1,11 @@
 function showCustomMinutesField(){
-    var dropdown = document.getElementById(optionsSelect);
-    var textInput = document.getElementById(minutesInput);
-
-    if (optionsSelect === "Other"){
-        minutesInput.style.display = "block";
+    const dropdown = document.getElementById("optionsSelect");
+    const textInput = document.getElementById("minutesInput");
+    if (dropdown.value === "Other"){
+        textInput.style.display = "block";
+        textInput.focus();
     } else{
         textInput.style.display = "none";
     }
 }
+// you may want to add validation to the input field
